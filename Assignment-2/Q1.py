6c08f7d0855f2bd3463ef1085feeb570e856d884
Sanskruti Saste <sanskrutisaste5@gmail.com>
2026-06-21 13:22:51 +0530

    Add files via upload

---
+++ Assignment-2/Q1.py
@@ -0,0 +1,8 @@
+x = 10
+print(type(x))         #type
+
+x = 10
+print(x)             #value
+
+x=10
+print(id(x))          #memory address
---
+++ Assignment-2/Q9.py
@@ -0,0 +1,7 @@
+print("enter your name :")
+Name = input()
+
+print("enter your age :")
+Age =(int(input()))
+
+print("Hello",Name + " you will turn",Age+1 ,"Next year")
